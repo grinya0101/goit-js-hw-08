e268ff4cc2bf0a1fecbc61ece2192b7ffa4977c2
grinya0101 <grinya1994015@gmail.com>
2022-07-16 22:16:53 +0300

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+// Дополнительный импорт стилей
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener('click', onContaiterClick)
+
+
+ 
+
+const imgGallery = galleryItems.map(({preview, original, description}) => {
+    console.log(galleryItems);
+    return `<div class="gallery__item"><a class="gallery__link" href="${original}" ><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></div>`
+
+    
+}).join(" ");
+
+gallery.insertAdjacentHTML("beforeend", ` ${imgGallery}`); 
+
+
+function onContaiterClick (evt){
+    evt.preventDefault()
+    if(!evt.target.classList.contains('gallery__image')){
+        return
+      } 
+      const instance = basicLightbox.create(`
+      <img src="${evt.target.dataset.source}">
+  `)
+      console.log(evt.target.dataset.sourse)
+      instance.show()
+
+
+}
