9361e3b01b4d660149672e1903208781713093ab
grinya0101 <grinya1994015@gmail.com>
2022-07-26 18:08:40 +0300

    hw-8-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,36 +8,50 @@ const STORAGGE_KEY = 'feedback-form-state';
 formEL.addEventListener('submit', onFormSubmit)
 formEL.addEventListener('input', throttle(onSaveStorage, 500));
 
+const formData = {};
 getStorageInputs();
 
-const formData ={};
-
-
 function onSaveStorage(e) {
-    formData[e.target.name] = e.target.value
-    localStorage.setItem(STORAGGE_KEY, JSON.stringify(formData))
+  let savedInputs = localStorage.getItem(STORAGGE_KEY);
+  // savedInputs = savedInputs ? JSON.parse(savedInputs) : {};
+  if(savedInputs){
+    savedInputs = JSON.parse(savedInputs)
+  } else{
+    savedInputs = {}
+  }
+  savedInputs[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGGE_KEY, JSON.stringify(savedInputs));
 }
 
 function getStorageInputs() {
     let savedData = localStorage.getItem(STORAGGE_KEY);
+    
     if (savedData) {
       savedData = JSON.parse(savedData);
       Object.entries(savedData).forEach(([name, value]) => {
         formEL.elements[name].value = value;
+        console.log(formData)
       });
     }
   }
 
 function onFormSubmit(evt) {
-    evt.preventDefault();
-    evt.currentTarget.reset();
-    localStorage.removeItem(STORAGGE_KEY)
+  
+  evt.preventDefault();
+
+  /*Для Вывода обьекта в консоль */
+  const formData = new FormData(formEL);
+  const userData = {};
+  formData.forEach((value, name) => userData[name] = value);
+  console.log(userData);
+  /*Проверка на заполнение всех полей */
+  const { email, message } = evt.target.elements;
+  if (email.value === "" || message.value === "") {
+   return
+  }
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGGE_KEY);
   }
   
 
-
-
-
-
-
-
+  
